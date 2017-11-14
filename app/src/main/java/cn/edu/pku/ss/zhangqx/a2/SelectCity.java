6c81a814b98224b05217bd563983ca0eae8f3d78
liubo <1701210361@pku.edu.cn>
2017-11-15 05:39:27 +0800

    2017.11.14

--- app/src/main/java/cn/edu/pku/ss/zhangqx/a2/SelectCity.java
+++ app/src/main/java/cn/edu/pku/ss/zhangqx/a2/SelectCity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 /**
  * Created by kk on 2017/10/18.
@@ -18,11 +19,22 @@ public class SelectCity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.select_city);
+        initViews();
+        listView = (ListView) findViewById(R.id.listView);
 
         mBackBtn=(ImageView) findViewById(R.id.title_back);
         mBackBtn.setOnClickListener(this);
     }
-
+    private void initViews(){
+        mBackBtn =(ImageView) findViewById(R.id.title_back);
+        mBackBtn.setOnClickListener(this);
+        mList();
+    }
+    private String[] data={"第1组","第2组","第3组","第4组","第5组","第6组",
+            "第7组","第8组","第9组","第10组","第11组","第12组","第13组",
+            "第14组","第15组","第16组","第17组","第18组","第19组","第20组",
+            "第21组","第22组"};
+    
     @Override
     public void onClick(View v) {
         switch(v.getId()) {
