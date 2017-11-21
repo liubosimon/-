b8523908df6d2ee7aefe11a74f75a48c82bf218d
liubo <1701210361@pku.edu.cn>
2017-11-21 13:14:31 +0800

    2017.11.21

--- app/src/main/java/cn/edu/pku/ss/zhangqx/a2/SelectCity.java
+++ app/src/main/java/cn/edu/pku/ss/zhangqx/a2/SelectCity.java
@@ -4,8 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.edu.pku.app.MyApplication;
+import cn.edu.pku.bean.City;
 
 /**
  * Created by kk on 2017/10/18.
@@ -17,24 +24,35 @@ public class SelectCity extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.select_city);
         initViews();
-        listView = (ListView) findViewById(R.id.listView);
 
-        mBackBtn=(ImageView) findViewById(R.id.title_back);
-        mBackBtn.setOnClickListener(this);
     }
+
     private void initViews(){
         mBackBtn =(ImageView) findViewById(R.id.title_back);
         mBackBtn.setOnClickListener(this);
-        mList();
+
+        MyApplication myApplication = (MyApplication)getApplication();
+        final List<City> mCityList =myApplication.getCityList();
+        ArrayList<String>mCityArrayList =new ArrayList<>();
+        for (int i=0;i<mCityArrayList.size();i++){
+            String cityName =mCityList.get(i).getCity();
+            mCityArrayList.add(cityName);
+        }
+        ListView mCityListLV =(ListView)findViewById(R.id.selectcity_listview);
+        ArrayAdapter<String>adapter=new ArrayAdapter<String>(SelectCity.this,android.R.layout.simple_lisst_item_1,mCityArrayList);
+        mCityListLV.setAdapter(adapter);
+        mCityListLV.setOnItemClickListener((parent, view, position, id) → {
+            Intent iNewCD = new Intent ();
+            iNewCD.putExtra("cityCode",mCityList.get(position).get(position).getNumber());
+            setResult(RESULT_OK,iNewCD);
+            finish();
+
+
+        });
     }
-    private String[] data={"第1组","第2组","第3组","第4组","第5组","第6组",
-            "第7组","第8组","第9组","第10组","第11组","第12组","第13组",
-            "第14组","第15组","第16组","第17组","第18组","第19组","第20组",
-            "第21组","第22组"};
-    
+
     @Override
     public void onClick(View v) {
         switch(v.getId()) {
